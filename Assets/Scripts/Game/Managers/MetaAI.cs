5b1e7150e631df8f6e920755d40914253d919daf
higashihara Takafumi <kamifuta010@outlook.jp>
2022-09-18 18:31:25 +0900

    メタAIの調整

--- Assets/Scripts/Game/Managers/MetaAI.cs
+++ Assets/Scripts/Game/Managers/MetaAI.cs
@@ -45,7 +45,7 @@ namespace Game.Managers
             var average = dethPosList.Average();
             var value = curve.Evaluate((average / 10) + 0.5f);
 
-            var result = (value - 0.5f) * 2;
+            var result = (value - 0.65f) * 4;
             return result;
         }
     }
