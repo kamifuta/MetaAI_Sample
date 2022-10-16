d4f1d52eff0e232f3523bc960851f97c6987f064
higashihara Takafumi <kamifuta010@outlook.jp>
2022-10-16 14:57:40 +0900

    データベース連携

---
+++ Assets/Scripts/DataSender.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using System;
+using Firebase.Database;
+
+public class DataSender : MonoBehaviour 
+{
+    private DatabaseReference refarence;
+    private int id;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        id = new System.Random().Next();
+        refarence = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    public async void SendData(int count, TensityData data)
+    {
+        string json = JsonUtility.ToJson(data);
+
+        refarence.Child("TensityData").Child(id.ToString()).Child(count.ToString()).SetRawJsonValueAsync(json);
+        //refarence.Child("name").SetValueAsync("aaa");
+    }
+
+    public void PushData(TensityData data)
+    {
+        string json = JsonUtility.ToJson(data);
+
+        var key = refarence.Push().Key;
+
+    }
+}
